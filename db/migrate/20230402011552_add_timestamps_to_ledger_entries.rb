6049b4ce455912f40895ee974138d6de2927e0fc
cwagrant <cwagrant@gmail.com>
2023-04-02 18:41:22 -0500

    Fix

--- db/migrate/20230402011552_add_timestamps_to_ledger_entries.rb
+++ db/migrate/20230402011552_add_timestamps_to_ledger_entries.rb
@@ -1,11 +1,5 @@
 class AddTimestampsToLedgerEntries < ActiveRecord::Migration[7.0]
   def change
     add_timestamps :ledger_entries, null: true
-
-    previous = 1.week.ago
-    ::LedgerEntries.update_all(created_at: previous, updated_at: previous)
-
-    change_column_null :ledger_entries, :created_at, false
-    change_column_null :ledger_entries, :updated_at, false
   end
 end
