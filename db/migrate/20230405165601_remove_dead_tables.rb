876e589d5038d0bccb2a44574c1ab99eeb40735a
cwagrant <cwagrant@gmail.com>
2023-04-05 12:48:23 -0500

    Fix

--- db/migrate/20230405165601_remove_dead_tables.rb
+++ db/migrate/20230405165601_remove_dead_tables.rb
@@ -3,8 +3,8 @@ class RemoveDeadTables < ActiveRecord::Migration[7.0]
     drop_table :ledger_entries
     drop_table :account_adjustments
     drop_table :rental_agreement_payments
-    drop_table :rental_agreement_terms
     drop_table :rental_agreement_term_values
+    drop_table :rental_agreement_terms
     drop_table :terms
   end
 end
